81aaf795ac034a76c4f7dbcc85b85feb4a430809
1 <wrasf175@naver.com>
2022-10-11 21:43:16 +0900

    feat : write페이지 로컬스토리지 설정, 코드 구조 리팩토링

--- week2/velog/write.js
+++ week2/velog/write.js
@@ -2,41 +2,64 @@
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
-const addTag =(e) => {
+const tagStorage = JSON.parse(window.localStorage.getItem('tags')) || [] ;
 
+const loadLocalstorage = ()=>{
+    tagStorage.map((tagName)=>{
+        const loadedTag = document.createElement('li');
+        loadedTag.classList.add('tag-item');
+        loadedTag.innerText = tagName;
+        $('.tags-list').appendChild(loadedTag);
+        loadedTag.addEventListener('click', deleteTag);
+    })
+}
 
+const saveToLocalStorage = () => {
+    window.localStorage.setItem('tags', JSON.stringify(tagStorage));
+}
+
+const deleteTag = (e) => {
+    e.target.remove();
+    const savedItemIndex = tagStorage.indexOf(e.target.innerText);
+    tagStorage.splice(savedItemIndex, 1);
+    saveToLocalStorage();
+}
+
+const addTag = (e) => {
+    e.preventDefault();
     if(e.keyCode === 13){
-        e.preventDefault();
-        if(!e.target.value.match(/^\S/g)){
-            alert('공백을 없애주세요.');
+        if(!e.target.value.match(/^\S/g) || tagStorage.includes(e.target.value)){
+            alert('이미 존재하는 태그이거나 빈 태그입니다.');
         }else{
             const tagListItem = document.createElement('li');
             tagListItem.innerText = `${e.target.value}`
             tagListItem.classList.add('tag-item');
             $('.tags-list').appendChild(tagListItem);
             tagListItem.addEventListener('click', deleteTag);
+            tagStorage.push(e.target.value);
+            saveToLocalStorage();
             e.target.value='';
         }
     }
 }
 
-const deleteTag = (e) => {
-    e.target.remove();
+const goToHome = () => {
+    location.href = 'velog.html';
 }
 
-const attachEvent = ({ Input, AllTagItems }) => {
+const attachEvent = ({ Input, moveToHome }) => {
     Input.addEventListener('keydown', addTag);
-    AllTagItems.forEach((item)=>item.addEventListener('click', deleteTag));
-    
-    
+    moveToHome.addEventListener('click', goToHome);
 };
+
 const handleTagEventManager = (items) => {
+    loadLocalstorage()
     attachEvent(items);
 };
+
 window.onload = () => {
     handleTagEventManager({
         Input:$('.tags-input'),
-        AllTagItems:$$('.tag-item'),
-        
+        moveToHome:$('.move-btn'),
     });
 };
