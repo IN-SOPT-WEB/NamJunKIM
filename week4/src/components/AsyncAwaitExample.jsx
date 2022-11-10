ce52ab35b98cb0ed2d24ce1670f557c42104c0d4
1 <wrasf175@naver.com>
2022-11-11 00:06:59 +0900

    init : 4주차 예제 코드 작성

--- week4/src/components/AsyncAwaitExample.jsx
+++ week4/src/components/AsyncAwaitExample.jsx
@@ -3,16 +3,15 @@ import { useEffect } from 'react';
 
 export default function AsyncAwaitExample() {
     useEffect(()=>{
-        getGithubProfile();
+        // getGithubProfile();
     },[]);
 
   return (
-    <div>뇸!</div>
+    <div>안녕하세요.</div>
   )
 }
 
-
-async function getGithubProfile() {
-    const response =await axios.get(`https://api.github.com/users/{아이디}`)
-    console.log('data', response.data);
-}
+// async function getGithubProfile() {
+//     const response =await axios.get(`https://api.github.com/users/{아이디}`)
+//     console.log('data', response.data);
+// }
